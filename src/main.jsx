eb6ef63f7a83003c757bf1f176376503515eabf3
Mishal <mishal0404@gmail.com>
2023-03-26 03:46:08 +0530

    finishing

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import Page from './Page'
 import Interface from "./Interface"
 import './index.css'
 import { CookiesProvider } from "react-cookie";
-import { BrowserRouter } from 'react-router-dom'
+import {HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-  <BrowserRouter>
+  <HashRouter>
   <CookiesProvider>
   <Interface/>
   </CookiesProvider>
-  </BrowserRouter>
+  </HashRouter>
   </>
 )
