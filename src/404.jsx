cfe41717c87b76c9f2b607061c375500a72dda2f
Mishal <mishal0404@gmail.com>
2023-03-30 16:09:26 +0530

    sdfsdf

--- src/404.jsx
+++ src/404.jsx
@@ -4,7 +4,7 @@ function Fof(){
     return (<>
           <div id="error">
   <div id="box"></div>
-  <h3>ERROR 500</h3>
+  <h3>ERROR 404</h3>
   <p>Things are a little <span>unstable</span> here.</p>
   <p>Congrats on finding this btw.</p>
 </div>
