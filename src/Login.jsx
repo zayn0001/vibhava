f92b14b7af64bd1f205a2a5e8d5231ab8047617d
Mishal <mishal0404@gmail.com>
2023-03-31 12:08:52 +0530

    adding dist

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
-import { collection, addDoc, getDocs, setDoc, doc } from "firebase/firestore";
+import { collection, addDoc, getDocs, setDoc, doc, collectionGroup } from "firebase/firestore";
 //import {db} from './firebase';
 import { Alert, Button, TextField } from '@mui/material';
-import { getDatabase, onValue, ref } from 'firebase/database';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+//import { getModularInstance } from '@firebase/util';
 //import { useEffect, useState } from 'react'
 
 
@@ -10,11 +11,21 @@ import { getDatabase, onValue, ref } from 'firebase/database';
 function Login(){
     //const [cookies, setCookie] = useCookies(['user']);
     const [username, setUsername] = useState("")
+    const [name, setName] = useState("")
+    const [mail, setMail] = useState("")
 
     const [login, setLogin] = useState({})
     const getfromstorage = () => JSON.parse(localStorage.getItem("login"));
 
     const [alert, setAlert] = useState()
+
+    function writeUserData() {
+      const db = getDatabase();
+      set(ref(db, 'userdata/'+username), {
+        name: name,
+        mail: mail,
+      }); 
+    }
     
     useEffect(() => {
       localStorage.setItem("login", JSON.stringify(login));
@@ -39,8 +50,11 @@ function Login(){
           )
         }
         else{
+          writeUserData()
           //setCookie('Name', username, { path: '/' });
           setLogin(login=>({...login, "Name":username}))
+          setLogin(login=>({...login, "mail":mail}))
+          setLogin(login=>({...login, "insta":name}))
           //setCookie("SignedIn", true, { path: '/' })
           setLogin(login=>({...login, "SignedIn":true}))
           window.location.reload(false)
@@ -55,8 +69,10 @@ function Login(){
 
     return(
        <>
-        <div style={{marginTop:20, display:'flex', justifyContent: 'space-between'}}>
-        <TextField label="Instagram ID" style={{maxWidth: 500, minWidth:200, width:'80%',borderRadius:'5px', background:'white'}} onChange={e=>setUsername(e.target.value)} variant='filled'/>
+        <div style={{marginTop:20, display:'flex', justifyContent: 'space-between',alignItems: 'flex-end',gap: 15, flexDirection:"column"}}>
+        <TextField label="Instagram ID" style={{maxWidth: 500, minWidth:200, width:'100%',borderRadius:'5px', background:'white'}} onChange={e=>setUsername(e.target.value)} variant='filled' required/>
+        <TextField label="Name" style={{maxWidth: 500, minWidth:200, width:'100%',borderRadius:'5px', background:'white'}} onChange={e=>setName(e.target.value)} variant='filled' required/>
+        <TextField label="E-mail" style={{maxWidth: 500, minWidth:200, width:'100%',borderRadius:'5px', background:'white'}} onChange={e=>setMail(e.target.value)} variant='filled' required/>
         <Button variant='outlined' onClick={handleclick} style={{minWidth: 40,minHeight: 40,maxWidth: 40,marginLeft: 'auto',borderRadius:'5px', background:'white',marginTop:"auto",marginBottom:"auto", maxHeight: 40}}>&#x2713;</Button>
         </div>
         {alert}
